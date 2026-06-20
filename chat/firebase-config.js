ef0f9a0ef2a0a790f9e47b9f517a0c8b8093297c
weiwenjun196 <wj1202zhy@outlook.com>
2026-06-20 21:28:55 +0800

    Add files via upload

---
+++ chat/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBvUEAPV1Sfw2NCLSyiiLk30FrYKP1pA-Y",
+  authDomain: "mysite-491e8.firebaseapp.com",
+  databaseURL: "https://mysite-491e8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mysite-491e8",
+  storageBucket: "mysite-491e8.firebasestorage.app",
+  messagingSenderId: "840336638207",
+  appId: "1:840336638207:web:2bea198b8a935048084cca",
+  measurementId: "G-S8DKWQFMJF"
+};
+
+const ALL_USERS = ["bob", "admin", "tangli"];
